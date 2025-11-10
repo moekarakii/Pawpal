ca44017a715ec8e896e706347873b65754653c59
Kevin <kcrapo@csumb.edu>
2025-11-10 14:48:46 -0800

    Prepping LostPetMapView for dynamic location implementation

--- PawPal/Views/Community/LostPetMapView.swift
+++ PawPal/Views/Community/LostPetMapView.swift
@@ -11,15 +11,28 @@ import MapKit
 import Firebase
 
 struct LostPetMapView: View {
+    
+    // This starts our default location centered on San Francisco
+    // Want to replace it with the user's location data if available
     @State private var cameraPosition: MapCameraPosition = .region(
         MKCoordinateRegion(
             center: CLLocationCoordinate2D(latitude: 37.7749, longitude: -122.4194),
             span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
         )
     )
-
+    
+    // array of lost pets fetched from data base (init)
     @State private var lostPets: [LostPet] = []
-
+    
+    // A shared Firestore reference to make it more intuitave for reuse
+    private let db = Firestore.firestore()
+    
+    // LocationManager object manages access to CoreLocation updates
+    @StateObject private var locationManager = LocationManager()
+    
+    // Used to confirm when we’ve successfully centered on the user
+    @State private var hasCenteredOnUser = false
+    
     var body: some View {
         Map(position: $cameraPosition) {
             ForEach(lostPets) { pet in
@@ -29,11 +42,13 @@ struct LostPetMapView: View {
         .onAppear(perform: fetchLostPets)
         .navigationTitle("Lost Pets Map")
     }
-
+    
+    
+    
     private func fetchLostPets() {
-        Firestore.firestore().collection("lost_pets").getDocuments { snapshot, error in
+        db.collection("lost_pets").getDocuments { snapshot, error in
             guard let documents = snapshot?.documents else { return }
-
+            
             self.lostPets = documents.compactMap { doc in
                 let data = doc.data()
                 guard
@@ -44,7 +59,7 @@ struct LostPetMapView: View {
                 else {
                     return nil
                 }
-
+                
                 return LostPet(id: doc.documentID, petName: name, description: desc, latitude: lat, longitude: lng)
             }
         }
